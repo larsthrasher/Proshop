685405bf0a300f3966233469c547592ed6e985cb
larsthrasher <thrasherlars@gmail.com>
2020-11-10 17:49:04 -0500

    shopping cart

---
+++ backend/controllers/userController.js
@@ -0,0 +1,10 @@
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+
+
+//@desc Auth user & get token
+//@route GET /api/users/login
+// @access Public
+const authUser = asyncHandler(async (req, res) => {
+
+})
